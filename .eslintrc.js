5dfd9dbb8a86a722282a5c804d4c26d1bc1a0ec9
Thedark1337 <thedark1337@thedark1337.com>
2017-02-07 01:36:53 -0600

    Begin working on ES2015 transformation of code, fix a few linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,10 @@ module.exports = {
         'no-negated-condition': 0,
         'no-sync': 0,
         'no-var': 0,
-        'object-shorthand': 0,
+        'one-var': [2, {
+            initialized: 'never'
+        }],
         'prefer-arrow-callback': 0,
         'prefer-spread': 0,
-        'prefer-template': 0
     }
 };
